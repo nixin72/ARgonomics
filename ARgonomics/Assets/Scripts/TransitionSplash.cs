fafec9ecb3cc1912c98aeb6364c243aa84b06168
Neeham <Neehamk@gmail.com>
2020-01-26 01:00:30 -0500

    Transition Scene fixed.

---
+++ ARgonomics/Assets/Scripts/TransitionSplash.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionSplash : MonoBehaviour
+{
+    public float delayBeforeLoading = 10f;
+    public string sceneNameToLoad;
+    public float timeElapsed;
+
+    // Update is called once per frame
+    public void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed > delayBeforeLoading)
+        {
+            SceneManager.LoadScene(sceneNameToLoad);
+        }
+    }
+}
