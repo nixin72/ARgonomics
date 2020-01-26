32936071969427f10b625810fd2df327daf15776
Neeham <Neehamk@gmail.com>
2020-01-26 09:58:59 -0500

    Auto stash before merge of "Neeham" and "origin/Neeham"

---
+++ ARgonomics/Assets/Scripts/mindfulnessRandomText.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class mindfulnessRandomText : MonoBehaviour
+{
+    public Text mytext = null;
+    string[] textArray = { "Take a 15 minute walk.", "Medidate.", "Eat a fruit.", "Take a nap.", "Take a deep breath.", "Drink water.", "Go for a jog.", "Eat a fruit.", "Listen to: You make my dreams come true by Hall and Oates.", "Give a beautiful smile!"};
+    // Start is called before the first frame update
+    void Start()
+    {
+        int randomNumber = Random.Range(0, 10);
+        mytext.text = textArray[randomNumber];
+    }
+}
